01baeeb46cad8d1cd3aecd579d9a7e2200f50b99
LAB12_USER ON WS15 <L12Y19W15@LabHCI.net>
2025-09-11 20:55:06 +0800

    Added files not yet done

---
+++ jumawan-clydenixon/campusequipmentloan/src/main/java/edu/cit/jumawan/clydenixon/campusequipmentloan/dto/EquipmentDTO.java
@@ -0,0 +1,8 @@
+package edu.cit.jumawan.clydenixon.campusequipmentloan.dto;
+public class EquipmentDTO {
+    private Long id;
+    private String name;
+    private String type;
+    private String serialNumber;
+    // getters and setters
+}
---
+++ jumawan-clydenixon/campusequipmentloan/src/main/java/edu/cit/jumawan/clydenixon/campusequipmentloan/dto/ReturnLoanResponseDTO.java
@@ -0,0 +1,7 @@
+package edu.cit.jumawan.clydenixon.campusequipmentloan.dto;
+public class ReturnLoanResponseDTO {
+    private Long loanId;
+    private double penalty;
+    private String status;
+    // getters and setters
+}
