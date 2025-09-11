84e8bb15d8c7f74354d12a3df7bf156ab5e1af56
LAB12_USER ON WS23 <L12Y19W23@LabHCI.net>
2025-09-11 20:36:11 +0800

    Added system folders with files

---
+++ zafra-jake/src/test/java/edu/cit/zafra/jake/campusequipmentloan/CampusequipmentloanApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.zafra.jake.campusequipmentloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusequipmentloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
