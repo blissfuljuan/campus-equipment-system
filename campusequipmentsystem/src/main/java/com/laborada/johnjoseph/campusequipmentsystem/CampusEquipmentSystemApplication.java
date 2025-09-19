c2616ca2a46a32436c2c8182bfe53d5a3f8bd26e
sep <laboradajohnji@gmail.com>
2025-09-19 21:51:47 +0800

    Add Spring Boot project and update .gitignore

---
+++ campusequipmentsystem/src/main/java/com/laborada/johnjoseph/campusequipmentsystem/CampusEquipmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.laborada.johnjoseph.campusequipmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEquipmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusEquipmentSystemApplication.class, args);
+	}
+
+}
